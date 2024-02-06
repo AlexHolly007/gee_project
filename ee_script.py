4a7a7bfba750d58a4d756b56d20f7f154af78b71
Alex Holly <hollya@oregonstate.edu>
2024-02-06 13:14:57 -0800

    updates ee_script

--- ee_script.py
+++ ee_script.py
@@ -5,45 +5,45 @@ import urllib
 from os.path import expanduser
 import argparse
 
-# This is a test to see that my commits are being sent to the main branch - Cam
+def create_image():
+    # Define your region of interest (ROI)
+    # Replace the coordinates with the geometry of your ROI
+    roi = ee.Geometry.Polygon(
+        [[[-105.023104228107,40.49671745990021], [-105.023104228107,40.62452341627386], [-105.19751218709138,40.62452341627386], [-105.19751218709138,40.49671745990021]]]
+    )
 
-# Initialize the Earth Engine API
-ee.Authenticate()
-ee.Initialize(project='ee-hollya')
+    # Define the date range
+    start_date = '2020-01-01'
+    end_date = '2021-01-01'
 
-# Define your region of interest (ROI)
-# Replace the coordinates with the geometry of your ROI
-roi = ee.Geometry.Polygon(
-    [[[-105.023104228107,40.49671745990021], [-105.023104228107,40.62452341627386], [-105.19751218709138,40.62452341627386], [-105.19751218709138,40.49671745990021]]]
-)
-
-# Define the date range
-start_date = '2020-01-01'
-end_date = '2021-01-01'
-
-# Create a Landsat 8 image collection and apply filters
-L8 = ee.ImageCollection("LANDSAT/LC08/C02/T1_TOA") \
+    # Create a Landsat 8 image collection and apply filters
+    L8 = ee.ImageCollection("LANDSAT/LC08/C02/T1_TOA") \
     .filterBounds(roi) \
     .filterDate(start_date, end_date) \
     .filterMetadata("CLOUD_COVER", 'less_than', 1) \
     .mean()
 
-# Clip the image to the ROI
-L8_clip = L8.clip(roi)
+    # Clip the image to the ROI
+    L8_clip = L8.clip(roi)
 
-# Display the image
-visualization = L8_clip.visualize(bands=["B4", "B3", "B2"], min=0, max=0.3, gamma=1.4)
-Image(url=visualization.getThumbURL())
+    # Display the image
+    visualization = L8_clip.visualize(bands=["B4", "B3", "B2"], min=0, max=0.3, gamma=1.4)
+    Image(url=visualization.getThumbURL())
 
-# Save the image to local machine
-local_image_path = os.path.expanduser('output.png')
-visualization.getThumbURL(params={'region': roi.getInfo()['coordinates'], 'format': 'png', 'dimensions': 512})
-urllib.request.urlretrieve(visualization.getThumbURL(params={'region': roi.getInfo()['coordinates'], 'format': 'png', 'dimensions': 512}),
-                           local_image_path)
+    # Save the image to local machine
+    local_image_path = os.path.expanduser('output.png')
+    visualization.getThumbURL(params={'region': roi.getInfo()['coordinates'], 'format': 'png', 'dimensions': 512})
+    urllib.request.urlretrieve(visualization.getThumbURL(params={'region': roi.getInfo()['coordinates'], 'format': 'png', 'dimensions': 512}),
+                        local_image_path)
 
-print(f"Image saved locally at: {local_image_path}")
+    print(f"Image saved locally at: {local_image_path}")
 
-#if __name__ == "__main__":
+if __name__ == "__main__":
+    # Initialize the Earth Engine API
+    ee.Authenticate()
+    ee.Initialize(project='ee-hollya')
+
+    create_image()
     #parser = argparse.ArgumentParser(description="This script runs inference on aois within a .csv file.")
     #parser.add_argument("-d", "--dataset_dir", type=str, required=True, help="Directory containing the hr_dataset, and lr_dataset, in the correct format(see worldstrat github)")
     #args = parser.parse_args()
