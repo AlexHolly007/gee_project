b26030b5f20f9973bdfc0976c0e47ddc5baf6925
Alex Holly <hollya@oregonstate.edu>
2024-02-13 13:27:19 -0800

    updated ee_script

--- ee_script.py
+++ ee_script.py
@@ -12,68 +12,75 @@ ee.Initialize(project='ee-hollya')
 
 
 def create_image():
-    #roi = ee.Geometry.Polygon(
-    #    [[[-105.023104228107,40.49671745990021], [-105.023104228107,40.62452341627386], [-105.19751218709138,40.62452341627386], [-105.19751218709138,40.49671745990021]]]
-    #)
+    roi = ee.Geometry.Polygon(
+        [[[-105.023104228107,40.49671745990021], [-105.023104228107,40.62452341627386], [-105.19751218709138,40.62452341627386], [-105.19751218709138,40.49671745990021]]]
+    )
 
     # Define the date range
     start_date = '2019-07-01'
-    end_date = '2019-07-31'
+    end_date = '2020-07-01'
 
     # Create a Landsat 8 image collection and apply filters
-
-    # "LANDSAT/LC08/C02/T1_TOA" for normal
     basemap = (
         ee.ImageCollection("LANDSAT/LC08/C02/T1_TOA") \
-        #.filterBounds(roi) \
+        .filterBounds(roi) \
         .filterDate(start_date, end_date) \
         .filterMetadata("CLOUD_COVER", 'less_than', 1) \
         .mean()
     )
     L8 = (
-    ee.ImageCollection('ECMWF/ERA5/DAILY')
-    #.filterBounds(roi)
-    .select('total_precipitation')
-    .filter(ee.Filter.date(start_date, end_date))
-    .mean()
-)
-
+        ee.ImageCollection('ECMWF/ERA5/DAILY')
+        .filterBounds(roi)
+        .select('total_precipitation')
+        .filter(ee.Filter.date(start_date, end_date))
+        .mean()
+    )
     #base_clip = basemap.clip(roi)
     #L8_clip = L8.clip(roi)
 
     
     # Display the image
-    #visualization = L8_clip.visualize(bands=["B4", "B3", "B2"], min=0, max=0.3, gamma=1.4)
-    visualization = basemap.visualize(
-        bands=["B4", "B3", "B2"],
-        min=0,
-        max=0.3,
-        gamma=1.4
-    ).blend(
-        L8.visualize(
-            min=0,
-            max=0.02,
-            palette= ['ffffff', '00ffff', '0080ff', 'da00ff', 'ffa400', 'ff0000']
-        )
+    visualization = basemap.visualize(bands=["B4", "B3", "B2"], min=0, max=0.3, gamma=1.4)
+    #visualization = basemap.visualize(
+    #    bands=["B4", "B3", "B2"],
+    #    min=0,
+    #    max=0.3,
+    #    gamma=1.4
+    #    ).blend(
+    #        L8.visualize(
+    #            min=0,
+    #            max=0.002,
+    #            palette= ['ffffff', '00ffff', '0080ff', 'da00ff', 'ffa400', 'ff0000']
+    #        )
+    #    )
+    export_task = ee.batch.Export.image.toCloudStorage(
+        image=visualization,
+        description='exported_image',
+        bucket="ee-hollya",
+        fileNamePrefix="image.png",
+        region=visualization.geometry().bounds(),
+        scale=30  # Adjust the scale as needed
     )
+    export_task.start()
 
     # Save the image to local machine
     local_image_path = os.path.expanduser('output.png')
+    print("TESTING")
     urllib.request.urlretrieve(
                     visualization.getThumbURL(
                         params={
-                            #'region': roi.getInfo()['coordinates'],
+                            'region': roi.getInfo()['coordinates'],
                             'format': 'png',
                             'dimensions': 512
                         }
                     ),
                     local_image_path
-                )
+    )
 
     print(f"Image saved locally at: {local_image_path}")
 
-#if __name__ == "__main__":
-create_image()
+if __name__ == "__main__":
+    create_image()
     #parser = argparse.ArgumentParser(description="This script runs inference on aois within a .csv file.")
     #parser.add_argument("-d", "--dataset_dir", type=str, required=True, help="Directory containing the hr_dataset, and lr_dataset, in the correct format(see worldstrat github)")
     #args = parser.parse_args()
