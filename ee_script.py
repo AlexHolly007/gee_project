f1d648d101f3cab4ed387d3005660d790e0f7e9c
Alex Holly <hollya@oregonstate.edu>
2024-02-15 13:02:21 -0800

    ee_script update

--- ee_script.py
+++ ee_script.py
@@ -11,21 +11,23 @@ ee.Initialize(project='ee-hollya')
 
 
 
-def create_image():
+def create_image(start_date, end_date, long, lat, miles):
+    deg_long = miles/55
+    deg_lat = miles/69
+    TL = [long+deg_long, lat-deg_lat]
+    TR = [long+deg_long, lat+deg_lat]
+    BL = [long-deg_long, lat+deg_lat]
+    BR = [long-deg_long, lat-deg_lat]
     roi = ee.Geometry.Polygon(
-        [[[-105.023104228107,40.49671745990021], [-105.023104228107,40.62452341627386], [-105.19751218709138,40.62452341627386], [-105.19751218709138,40.49671745990021]]]
+        [[TL, TR, BL, BR]]
     )
-
-    # Define the date range
-    start_date = '2019-07-01'
-    end_date = '2020-07-01'
 
     # Create a Landsat 8 image collection and apply filters
     basemap = (
         ee.ImageCollection("LANDSAT/LC08/C02/T1_TOA") \
         .filterBounds(roi) \
         .filterDate(start_date, end_date) \
-        .filterMetadata("CLOUD_COVER", 'less_than', 1) \
+        .filterMetadata("CLOUD_COVER", 'less_than', .3) \
         .mean()
     )
     L8 = (
@@ -35,7 +37,7 @@ def create_image():
         .filter(ee.Filter.date(start_date, end_date))
         .mean()
     )
-    basemap = basemap.clip(roi)
+    #basemap = basemap.clip(roi)
     L8 = L8.clip(roi)
     visualization = basemap.visualize(bands=["B4", "B3", "B2"], min=0, max=0.3, gamma=1.4)
 
@@ -59,7 +61,7 @@ def create_image():
         image=visualization,
         description='exported_image',
         bucket="ee-hollya",
-        fileNamePrefix="image",
+        fileNamePrefix=f"Timelapse/image",
         region=visualization.geometry().bounds(),
         scale=30  # Adjust the scale as needed
     )
@@ -82,7 +84,12 @@ def create_image():
     print(f"Image saved locally at: {local_image_path}")
 
 if __name__ == "__main__":
-    create_image()
+    start_date = '2019-07-01'
+    end_date = '2020-07-01'
+    lat = 40.5853
+    long = -105.0844
+    miles = 500
+    create_image(start_date, end_date, long, lat, miles)
     #parser = argparse.ArgumentParser(description="This script runs inference on aois within a .csv file.")
     #parser.add_argument("-d", "--dataset_dir", type=str, required=True, help="Directory containing the hr_dataset, and lr_dataset, in the correct format(see worldstrat github)")
     #args = parser.parse_args()
