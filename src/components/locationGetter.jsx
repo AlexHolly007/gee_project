b833e90c4837f8f8a31db772f7031c2545dfd2cc
canfieca <canfieca@oregonstate.edu>
2024-02-22 16:52:01 -0800

    newCommit

--- src/components/locationGetter.jsx
+++ src/components/locationGetter.jsx
@@ -1,20 +1,70 @@
-import React from 'react'
-import '../css/locationGetter.css'
+// import React from 'react'
+// import '../css/locationGetter.css'
+
+// function LocationGetter() {
+//     return (
+//         <>
+//             <div className="location--container">
+//                 <form className="user--input">
+//                     <label htmlFor="longitude">Longitude: </label>
+//                     <input type="text" id="longitude" name="longitude"></input>
+//                     <label htmlFor="latitude">Latitude: </label>
+//                     <input type="text" id="latitude" name="latitude"></input>
+//                     <label htmlFor="miles">Miles: </label>
+//                     <input type="text" id="miles" name="miles"></input>
+//                     <button type="submit">Submit</button>
+//                 </form>
+//             </div>
+//             <h1>This is where the location will be printed out after user enters</h1>
+//         </>
+//     )
+// }
+
+// export default LocationGetter;
+
+import React, { useState } from 'react';
+import '../css/locationGetter.css';
 
 function LocationGetter() {
+    const [longitude, setLongitude] = useState('');
+    const [latitude, setLatitude] = useState('');
+    const [miles, setMiles] = useState('');
+    const [locationResult, setLocationResult] = useState('');
+
+    const handleSubmit = async (event) => {
+        event.preventDefault();
+        try {
+            const response = await fetch(`http://127.0.0.1:60061/getTimelapse/?lon=${longitude}&lat=${latitude}&miles=${miles}`, {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify({ longitude, latitude, miles })
+            });
+            const data = await response.json();
+            setLocationResult(data.result); // Assuming API sends back a result
+        } catch (error) {
+            console.error('Error:', error);
+            setLocationResult('Error occurred while fetching data');
+        }
+    };
+
     return (
         <>
             <div className="location--container">
-                <form className="user--input">
+                <form className="user--input" onSubmit={handleSubmit}>
                     <label htmlFor="longitude">Longitude: </label>
-                    <input type="text" id="longitude" name="longitude"></input>
+                    <input type="text" id="longitude" name="longitude" value={longitude} onChange={(e) => setLongitude(e.target.value)}></input>
                     <label htmlFor="latitude">Latitude: </label>
-                    <input type="text" id="latitude" name="latitude"></input>
+                    <input type="text" id="latitude" name="latitude" value={latitude} onChange={(e) => setLatitude(e.target.value)}></input>
+                    <label htmlFor="miles">Miles: </label>
+                    <input type="text" id="miles" name="miles" value={miles} onChange={(e) => setMiles(e.target.value)}></input>
+                    <button type="submit" onClick={handleSubmit}>Submit</button>
                 </form>
             </div>
-            <h1>This is where the location will be printed out after user enters</h1>
+            <h1>{locationResult}</h1>
         </>
-    )
+    );
 }
 
 export default LocationGetter;
--- src/components/timeCard.jsx
+++ src/components/timeCard.jsx
@@ -1,15 +1,53 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import '../css/timeCard.css'
 
-export default function timeCard(props) {
+/**
+ * function getPictureStyle() {
+        fetch('/make_request', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(''),
+        })
+        .then(response => response.json())
+        .then(data => {
+            picture_style = data['result'];// Saving the picture style for further use. It is a global variable.
+        })
+        .catch(error => { //error checking
+            responseContainer.innerHTML = error;
+            console.error('Error:', error);
+        });
+    }
+    getPictureStyle();
+ * 
+ */
 
-    const { image } = props
+export default function timeCard() {
 
+    const [images, setImages] = useState([]);
+    const [index, setIndex] = useState(0);
+
+    useEffect(() => {
+        // Fetch list of image names from backend route
+        // try {
+        //     const response = await fetch(
+        //         `http://127.0.0.1:60061/getTimelapse/?=${}`
+        //     )
+        // }
+    }, []);
+
+    useEffect(() => {
+        const interval = setInterval(() => {
+            setIndex(i => (i + 1) % images.length);
+        }, 100);
+        return () => clearInterval(interval);
+    }, [images]);
 
     return (
         <>
             <div className='timeContainer'>
-                <img className="bigCard" src={image}>
+                <img className="bigCard" src={`/images/${images[index]}`}>
                 </img>
             </div>
         </>
